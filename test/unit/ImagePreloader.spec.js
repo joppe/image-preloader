4986fe6015de23f15c64b885a56a0eb58566f8aa
Joppe Aarts <joppe@apestaartje.info>
2015-09-29 19:46:24 +0200

    ImagePreloader test added

---
+++ test/unit/ImagePreloader.spec.js
@@ -0,0 +1,76 @@
+/*global describe, it, expect, beforeEach, loadFixtures*/
+
+import {ImagePreloader} from 'image-preloader/ImagePreloader';
+
+describe('ImagePreloader', function () {
+    it('Allows only HTMLCollection/Array/HTMLImageElement/String as argument for the constructor', function () {
+        expect(function () {
+            let l = new ImagePreloader(null);
+        }).toThrow();
+
+        expect(function () {
+            new ImagePreloader(2);
+        }).toThrow();
+
+        expect(function () {
+            new ImagePreloader(new Date());
+        }).toThrow();
+    });
+
+    it('Allows only HTMLCollection/Array/HTMLImageElement/String as argument for addLoadable', function () {
+        expect(function () {
+            let loader = new ImagePreloader();
+            loader.addLoadable(null);
+        }).toThrow();
+
+        expect(function () {
+            let loader = new ImagePreloader();
+            loader.addLoadable('http://lorempixel.com/400/200/sports/');
+        }).not.toThrow();
+
+        expect(function () {
+            let image= new Image(),
+                loader = new ImagePreloader();
+
+            image.setAttribute('src', 'http://lorempixel.com/400/200/sports/');
+            loader.addLoadable(image);
+        }).not.toThrow();
+    });
+});
+
+describe('ImagePreloader', function () {
+    jasmine.getFixtures().fixturesPath = 'base/test/fixtures';
+
+
+    let loader,
+        l = 0,
+        e = 0,
+        p = 0;
+
+    beforeEach(function (done) {
+        loadFixtures('multiple.html');
+
+        loader = new ImagePreloader();
+        loader.addLoadable(document.getElementsByTagName('img'));
+        loader.load({
+            progress() {
+                p += 1;
+            },
+            load() {
+                l += 1;
+            },
+            error() {
+                e += 1;
+            },
+            complete() {
+                done();
+            }
+        });
+    }, 10000);
+
+    it('Load and listen to events', function () {
+        expect(p).toBe(5);
+        expect(e).toBe(0);
+        expect(l).toBe(5);
+    });
+});
